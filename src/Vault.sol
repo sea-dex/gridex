8c6ce507819c037fc6b973bad5e127c27a60d0a7
seaprotocol <seaprotocol@seadex.org>
2026-02-07 16:24:13 +0800

    fix: [H-02] Vault.sol Has No Withdrawal Mechanism

--- src/Vault.sol
+++ src/Vault.sol
@@ -2,7 +2,23 @@
 pragma solidity ^0.8.28;
 
 import {Owned} from "solmate/auth/Owned.sol";
+import {ERC20} from "solmate/tokens/ERC20.sol";
+import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
 
 contract Vault is Owned {
+    using SafeTransferLib for ERC20;
+
     constructor() Owned(msg.sender) {}
+
+    function withdrawERC20(address token, address to, uint256 amount) external onlyOwner {
+        ERC20(token).safeTransfer(to, amount);
+    }
+
+    // forge-lint: disable-next-line(mixed-case-function)
+    function withdrawETH(address to, uint256 amount) external onlyOwner {
+        (bool success,) = to.call{value: amount}("");
+        require(success, "ETH transfer failed");
+    }
+    
+    receive() external payable {}
 }
