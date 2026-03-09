80319bc169ed78f3d69befb95deec722334641b3
seaprotocol <seaprotocol@seadex.org>
2026-03-09 15:33:29 +0800

    update solc version

--- src/GridEx7702BatchExecutor.sol
+++ src/GridEx7702BatchExecutor.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.33;
 
 /// @title GridEx7702BatchExecutor
 /// @notice Minimal ERC-7821 executor intended for EIP-7702 delegated EOAs.
